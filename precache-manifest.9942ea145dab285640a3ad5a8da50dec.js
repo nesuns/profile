a7d00b3a51025dee0627ef506963026b811cec39
teth0r <teth0r>
2020-01-10 20:49:40 +0300

    Updates

---
+++ precache-manifest.9942ea145dab285640a3ad5a8da50dec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996e1676be0f9c4adb9e887dc8e9246d",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "e5224ae571a7836799d8",
+    "url": "/profile/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "e5224ae571a7836799d8",
+    "url": "/profile/static/js/2.1ca04b78.chunk.js"
+  },
+  {
+    "revision": "ca261ebf46f1ad523edd",
+    "url": "/profile/static/js/main.b088c1c3.chunk.js"
+  },
+  {
+    "revision": "08328a0b3f2cfb28bb1b",
+    "url": "/profile/static/js/runtime-main.51060d0a.js"
+  }
+]);
